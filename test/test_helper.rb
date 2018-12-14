3c46a5f40edaa1dfcb92c1f320733bd6aa1af751
Mark Modak <mmodak@mmodak.local>
2018-12-13 20:03:29 -0700

    Test helper created.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'bigdecimal'
+require 'pry'
+
+#require '../lib/item'
+#require '../lib/merchant'
+#require '../lib/merchant_repository'
+#require '../lib/item_repository'
