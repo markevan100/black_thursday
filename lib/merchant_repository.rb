f82e82a753fe0e1cf648ef55300186458f02644f
Mark Modak <mmodak@mmodak.local>
2018-12-13 21:36:18 -0700

    Merchant_repository build out with 3 find methods and a create method along with appropriate testing.

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -13,4 +13,46 @@ class MerchantRepository
       @merchants << merchant
     end
   end
+
+  def all
+    @merchants
+  end
+
+  def find_by_id(id)
+    @merchants.find do |m|
+      m[:id] == id
+    end
+  end
+
+  def find_by_name(name)
+    @merchants.find do |m|
+      m[:name] == name
+    end
+  end
+
+  def find_all_by_name(name)
+    array = []
+    mer = @merchants.find_all do |m|
+      array << m[:name] if m[:name].include?(name)
+    end
+    array
+  end
+
+  def current_id_max
+    highest = @merchants.max_by do |m|
+      m[:id]
+    end
+    highest[:id]
+  end
+
+  def create(name)
+    num = self.current_id_max + 1
+    new_one = Merchant.new
+      new_one[:id] = num
+      new_one[:name] = name
+      new_one[:created_at] = Time.now.strftime("%m/%d/%Y")
+      new_one[:updated_at] = Time.now.strftime("%m/%d/%Y")
+
+      @merchants << new_one
+  end
 end
--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -2,26 +2,68 @@ require_relative 'test_helper'
 
 class MerchantTest < Minitest::Test
 
+  def setup
+    @merchant = Merchant.new
+    @new_respository = MerchantRepository.new
+  end
+
   #These are tests for the merchant instance
   def test_merchant_exists
-    merchant = Merchant.new
-    assert_instance_of Merchant, merchant
+    assert_instance_of Merchant, @merchant
   end
 
   def test_merchant_is_a_hash
-    merchant = Merchant.new
-    merchant[:id] = 123456
-    assert_equal 123456, merchant[:id]
+    @merchant[:id] = 123456
+    assert_equal 123456, @merchant[:id]
   end
 
   #These are tests for the merchant_repository class
   def test_merchant_respository_exists
-    merchants = MerchantRepository.new
-    assert_instance_of MerchantRepository, merchants
+    assert_instance_of MerchantRepository, @new_respository
   end
 
   def test_merchant_respository_has_a_merchant_attribute
-    new_respository = MerchantRepository.new
-    assert_equal 12334105, new_respository.merchants[0][:id]
+    assert_equal 12334105, @new_respository.merchants[0][:id]
   end
+
+  #These test methods on the merchant_repository class
+  def test_find_by_id
+    one_merch = @new_respository.find_by_id(12336739)
+    assert_equal "RanaParvaShop", one_merch[:name]
+  end
+
+  def test_find_by_name
+    one_merch = @new_respository.find_by_name("RanaParvaShop")
+    assert_equal 12336739, one_merch[:id]
+  end
+
+  def test_find_all_by_name
+    assert_equal ["9LivesJewelry", "MarkThomasJewelry"], @new_respository.find_all_by_name("sJewelry")
+  end
+
+  def test_find_all_by_name_no_matches_returns_empty_array
+    assert_equal [], @new_respository.find_all_by_name("thisdoesntexist")
+  end
+
+  def test_current_highest_id
+    assert_equal 12337411, @new_respository.current_id_max
+  end
+
+  #These are tests for the create method of the merchant_repository class
+  def test_create_creates_another_merchant_instance
+    assert_equal 475, @new_respository.merchants.count
+    @new_respository.create("Mark's Pinatas")
+    assert_equal 476, @new_respository.merchants.count
+  end
+
+  def test_create_new_instance_can_be_found
+    assert_equal [], @new_respository.find_all_by_name("MarksPinatas")
+    @new_respository.create("MarksPinatas")
+    one_merch = @new_respository.find_by_name("MarksPinatas")
+    assert_equal 12337412, one_merch[:id]
+  end
+
+
+
+
 end
