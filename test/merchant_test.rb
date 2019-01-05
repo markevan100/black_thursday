ed6f47124601d2d04505280e9a042f03cf8f112e
Mark Modak <mmodak@mmodak.local>
2019-01-05 12:23:09 -0700

    Setup in merchant_test.rb

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -9,27 +9,14 @@ class MerchantTest < Minitest::Test
         created_at: "2010-12-10",
         updated_at: "2011-12-04"
       }
-    merchant = Merchant.new(attributes)
-  end 
+    @merchant = Merchant.new(attributes)
+  end
 
   def test_merchant_exists
-    attributes = {
-        id: 12334105,
-        name: "Shopin1901",
-        created_at: "2010-12-10",
-        updated_at: "2011-12-04"
-      }
-    merchant = Merchant.new(attributes)
-    assert_instance_of Merchant, merchant
+    assert_instance_of Merchant, @merchant
   end
 
   def test_merchant_id
-    attributes = {
-        id: 12334105,
-        name: "Shopin1901",
-        created_at: "2010-12-10",
-        updated_at: "2011-12-04"
-      }
-    merchant = Merchant.new(attributes)
-    assert merchant.id
+    assert_equal 12334105, @merchant.id
+  end
 end
